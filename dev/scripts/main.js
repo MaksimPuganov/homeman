2b27e4375bf250a72a9e1ec32c0027b6304b1368
Maksim Puganov <aquariustech@yandex.ru>
2015-12-17 19:54:42 +0300

    remake the script of jQuery on JS

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-    $("[data-toggle]").click(function() {
-        var toggle_el = $(this).data("toggle");
-        $(toggle_el).toggleClass("open-sidebar");
-    });
+var showMenu = (function () {
 
-});
+    var init = function () {
+        _setUpListners();
+    };
+
+    var _setUpListners = function () {
+        // прослушка событий
+        document.getElementById("sidebar-toggle").addEventListener("click", toogleMenu);
+        function toogleMenu() {
+            document.querySelector(".wrapper").classList.toggle("open-sidebar");
+        };
+    }
+
+    return {
+        init : init
+    }
+})();
+
+function ready(fn) {
+    if (document.readyState != 'loading'){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+ready(showMenu.init);
